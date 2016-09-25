43eeafd2f29c62baa501c71a2ff5c837efbf482a
MichalParadis <paradis2@ualberta.ca>
2016-09-25 11:33:29 -0600

    Finished Basic UI now I need to code everything.

---
+++ app/src/main/java/com/example/michael/paradis2_habittracker/ModifyHabit.java
@@ -0,0 +1,13 @@
+package com.example.michael.paradis2_habittracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyHabit extends AppCompatActivity {
+//TODO IF anything breaks when it comes to dayofweek buttons create new ids for the modifying buttons.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_habit);
+    }
+}
---
+++ app/src/main/java/com/example/michael/paradis2_habittracker/ViewIndividualHabit.java
@@ -0,0 +1,13 @@
+package com.example.michael.paradis2_habittracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewIndividualHabit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_individual_habit);
+    }
+}
