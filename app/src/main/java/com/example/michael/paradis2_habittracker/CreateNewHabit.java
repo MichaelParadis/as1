fd8b689b2c49b258a40a43b7d863601df412e3fe
MichalParadis <paradis2@ualberta.ca>
2016-09-25 11:03:16 -0600

    Finished UI for Create New Habit I believe.

--- app/src/main/java/com/example/michael/paradis2_habittracker/CreateNewHabit.java
+++ app/src/main/java/com/example/michael/paradis2_habittracker/CreateNewHabit.java
@@ -1,7 +1,14 @@
 package com.example.michael.paradis2_habittracker;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.util.Date;
+
 
 public class CreateNewHabit extends AppCompatActivity {
 
@@ -9,5 +16,12 @@ public class CreateNewHabit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_habit);
+        // http://stackoverflow.com/questions/2008558/displaying-a-default-date-in-a-edittext-widget
+        EditText DateText = (EditText) findViewById(R.id.NewHabitDate);
+        DateText.setText(DateFormat.getDateInstance().format(new Date()) );
+    }
+    public void showDatePickerDialog(View v){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "habitDatePicker");
     }
 }
