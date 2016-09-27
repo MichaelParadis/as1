b7f73d99c08d5dd50a8eefd1c45975c8b2959123
MichalParadis <paradis2@ualberta.ca>
2016-09-27 10:40:14 -0600

    Finished adding tests for HabitTest.java

--- app/src/test/java/com/example/michael/paradis2_habittracker/HabitTest.java
+++ app/src/test/java/com/example/michael/paradis2_habittracker/HabitTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Date;
 
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 public class HabitTest {
@@ -31,7 +32,7 @@ public class HabitTest {
 
     }
     @Test
-    public void TestGetCompletions(){
+    public void TestEmptyGetCompletions(){
         String habitName = "Habit";
         Habit habit = new Habit(habitName);
         assertTrue("Habit not initialized correctly", habit.getCompletions() == 0);
@@ -41,6 +42,34 @@ public class HabitTest {
         Habit habit2 = new Habit(habitName, startDate);
         assertTrue("Habit with set start date not given correct completions", habit2.getCompletions() == 0);
         assertTrue("Habit with set start date not given correct completiondateslist", habit2.getCompletionDates().isEmpty());
+
+    }
+    @Test
+    public  void TestAddingCompletions(){
+
+        String habitName = "Habit";
+        Habit habit = new Habit(habitName);
+        habit.addCompletion();
+        assertTrue("Habit not incrementing completions", habit.getCompletions() == 1);
+        assertFalse("Habit not adding completion date", habit.getCompletionDates().isEmpty());
+
+        Date startDate = new Date();
+        Habit habit2 = new Habit(habitName, startDate);
+        habit2.addCompletion();
+        assertTrue("Habit with set start date not incrementing completions ", habit2.getCompletions() == 0);
+        assertFalse("Habit with set start date not adding completion date", habit2.getCompletionDates().isEmpty());
+    }
+    @Test
+    public void TestGettingMissedCompletions(){
+        String habitName = "Habit";
+        Habit habit = new Habit(habitName);
+        Habit habit2 = new Habit(habitName, new Date());
+        assertTrue("Habit does not set missed completions", habit.getMissedCompletions() == 0);
+        assertTrue("Habit with date does not set missed completions", habit2.getMissedCompletions() == 0);
+        habit.addMissedCompletion();
+        habit2.addMissedCompletion();
+        assertTrue("Habit does not add missed completions", habit.getMissedCompletions() == 1);
+        assertTrue("Habit with date does not add missed completions", habit2.getMissedCompletions() == 1);
 
     }
 
