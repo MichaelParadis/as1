5b104440d9f0f3da1b4ae7973a2c7596f81ae98a
MichalParadis <paradis2@ualberta.ca>
2016-10-03 09:43:21 -0600

    Added licences to files.

--- app/src/main/java/com/example/michael/paradis2_habittracker/DatePickerFragment.java
+++ app/src/main/java/com/example/michael/paradis2_habittracker/DatePickerFragment.java
@@ -20,7 +20,39 @@ import java.util.Locale;
  * Used from https://developer.android.com/guide/topics/ui/controls/pickers.html
  * to allow the setting of the date for the newhabit
  */
+/*
+        Android App that keeps track of your daily habits.
+        Copyright (C) 2016  Michael Paradis
 
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+        */
+
+/*
+            Copyright 2016 Michael Paradis, Android Documentation
+
+            Licensed under the Apache License, Version 2.0 (the "License");
+            you may not use this file except in compliance with the License.
+            You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+            Unless required by applicable law or agreed to in writing, software
+            distributed under the License is distributed on an "AS IS" BASIS,
+            WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+            See the License for the specific language governing permissions and
+            limitations under the License.
+            */
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
     //EditText input = (EditText) getActivity().findViewById(R.id.NewHabitDate);
 
--- app/src/main/java/com/example/michael/paradis2_habittracker/HabitListController.java
+++ app/src/main/java/com/example/michael/paradis2_habittracker/HabitListController.java
@@ -8,7 +8,23 @@ import java.util.ArrayList;
  * Created by michael on 01/10/16.
  * Based on studentpicker
  */
+/*
+        Android App that keeps track of your daily habits.
+        Copyright (C) 2016  Michael Paradis
 
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+        */
 public class HabitListController {
     private static HabitList habitlist = null;
     public static HabitList getHabitlist(){
